a6212b3ace20d4b205278b33a4a3ff467072de77
michaellehmkuhl <michael@electricpulp.com>
2010-03-08 14:50:11 -0600

    * Added Twitter Search API capabilities

--- jquery.jtwitter.js
+++ jquery.jtwitter.js
@@ -1,16 +1,19 @@
 /*
- * jTwitter 1.1.1 - Twitter API abstraction plugin for jQuery
+ * jTwitter 1.1.2 - Twitter API abstraction plugin for jQuery
  *
  * Copyright (c) 2009 jQuery Howto
+ * Copyright (c) 2010 Electric Pulp
  *
  * Licensed under the GPL license:
  *   http://www.gnu.org/licenses/gpl.html
  *
  * URL:
  *   http://jquery-howto.blogspot.com
+ *   http://electricpulp.com
  *
  * Author URL:
  *   http://jquery-howto.blogspot.com
+ *   http://electricpulp.com
  *
  */
 (function( $ ){
@@ -30,6 +33,29 @@
 			var url = "http://twitter.com/status/user_timeline/"
 				+ username + ".json?count="+numPosts+"&callback=?";
 
+			$.getJSON( url, function( data ){
+				if( $.isFunction( fnk ) ) {
+					fnk.call( this, data );
+				}
+			});
+		}
+	});
+	$.extend( {
+		jTwitterSearch:function( query, numPosts, fnk) {
+			var info = {};
+			
+			// If no arguments are sent or only username is set
+			if( query == 'undefined' || numPosts == 'undefined' ) {
+				return;
+			} else if( $.isFunction( numPosts ) ) {
+				// If only username and callback function is set
+				fnk = numPosts;
+				numPosts = 5;
+			}
+			
+			if (!query.match(/^q=/)) query = 'q='+query;
+			var url = "http://search.twitter.com/search.json?&"+query+"&rpp="+numPosts+"&callback=?";
+console.log(url);
 			$.getJSON( url, function( data ){
 				if( $.isFunction( fnk ) ) {
 					fnk.call( this, data );
---
+++ output-demo-search.js
@@ -0,0 +1,53 @@
+/*
+    Demo output for $.jTwitterSearch('+%40electricpulp+OR+%22electric+pulp%22', 3) call
+*/
+
+{
+    "results": [
+		{
+	        "profile_image_url": "http://a1.twimg.com/profile_images/30469162/Photo_13_normal.jpg",
+	        "created_at": "Thu, 04 Mar 2010 15:38:46 +0000",
+	        "from_user": "jhummel",
+	        "to_user_id": null,
+	        "text": "Fargo vs. Sioux Falls Roller Derby this weekend. Any wagers @electricpulp?",
+	        "id": 9978220711,
+	        "from_user_id": 113130,
+	        "geo": null,
+	        "iso_language_code": "en",
+	        "source": "&lt;a href=&quot;http://www.atebits.com/&quot; rel=&quot;nofollow&quot;&gt;Tweetie&lt;/a&gt;"
+	    },
+	    {
+	        "profile_image_url": "http://a3.twimg.com/profile_images/67306907/large_icon_normal.jpg",
+	        "created_at": "Thu, 04 Mar 2010 14:58:58 +0000",
+	        "from_user": "carlawhite",
+	        "to_user_id": null,
+	        "text": "nice to see sioux falls news sharing  @electricpulp success. lotsa beards. http://bit.ly/aaNvSE",
+	        "id": 9976635906,
+	        "from_user_id": 4387179,
+	        "geo": null,
+	        "iso_language_code": "en",
+	        "source": "&lt;a href=&quot;http://www.tweetdeck.com/&quot; rel=&quot;nofollow&quot;&gt;TweetDeck&lt;/a&gt;"
+	    },
+	    {
+	        "profile_image_url": "http://a1.twimg.com/profile_images/588400572/4674_544255296274_41302122_32318407_5703482_n_normal.jpg",
+	        "created_at": "Thu, 04 Mar 2010 03:48:15 +0000",
+	        "from_user": "dletsche",
+	        "to_user_id": 844,
+	        "text": "@stefanhartwig @aaronmentele @electricpulp - hey, nice seeing ya on KDLT!",
+	        "id": 9956969969,
+	        "from_user_id": 48387,
+	        "to_user": "stefanhartwig",
+	        "geo": null,
+	        "iso_language_code": "en",
+	        "source": "&lt;a href=&quot;http://twitter.com/&quot;&gt;web&lt;/a&gt;"
+	    }
+	],
+    "max_id": 10183279212,
+    "since_id": 0,
+    "refresh_url": "?since_id=10183279212&q=+%40electricpulp+OR+%22electric+pulp%22",
+    "next_page": "?page=2&max_id=10183279212&rpp=3&q=+%40electricpulp+OR+%22electric+pulp%22",
+    "results_per_page": 3,
+    "page": 1,
+    "completed_in": 0.049848,
+    "query": "+%40electricpulp+OR+%22electric+pulp%22"
+}
